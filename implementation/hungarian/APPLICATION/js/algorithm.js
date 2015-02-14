4122c0f44af7093aa0b82b555ddc7bec90d0d04c
alex <avoroncovs@gmail.com>
2015-02-15 00:57:11 +0100

    Implemented replay steps

--- implementation/hungarian/APPLICATION/js/algorithm.js
+++ implementation/hungarian/APPLICATION/js/algorithm.js
@@ -1,152 +1,5 @@
-function setAll(arr, val) {
-    var i, n = arr.length;
-    for (i = 0; i < n; ++i) {
-        arr[i] = val;
-    }
-}
-//
-//function initLabels(){
-//    for (var x = 0; x < n; x++) {
-//        for (var y = 0; y < n; y++) {
-//            if(cost[x][y] > lx[x]){
-//                lx[x] = cost[x][y];
-//            }
-//        }
-//    }
-//}
-//
-//function augment(){
-//    if(maxMatch == cost.length) return;
-//    var x, y, root = -1;
-//    var q = new Array(n);
-//    var wr = 0, rd = 0;
-//    setAll(S, false);
-//    setAll(T, false);
-//    setAll(prev, -1);
-//    for (x = 0; x < n; x++) {
-//        if (xy[x] == -1) {
-//            q[wr++] = root = x;
-//            prev[x] = -2;
-//            S[x] = true;
-//            break;
-//        }
-//    }
-//
-//    for (y = 0; y < n; y++) {
-//        slack[y] = lx[root] + ly[y] - cost[root][y];
-//        slackx[y] = root;
-//    }
-//
-//    while (true){
-//        while (rd < wr) {
-//            x = q[rd++];
-//            for (y = 0; y < n; y++) {
-//                if (cost[x][y] == lx[x] + ly[y] && !T[y]) {
-//                    if (yx[y] == -1) break;
-//                    T[y] = true;
-//                    q[wr++] = yx[y];
-//                    add_to_tree(yx[y], x);
-//                }
-//            }
-//            if (y < n) break;
-//        }
-//        if (y < n) break;
-//
-//        update_labels();
-//        wr = rd = 0;
-//        for (y = 0; y < n; y++)
-//            if (!T[y] &&  slack[y] == 0) {
-//                if (yx[y] == -1) {
-//                    x = slackx[y];
-//                    break;
-//                }else {
-//                    T[y] = true;
-//                    if (!S[yx[y]]) {
-//                        q[wr++] = yx[y];
-//                        add_to_tree(yx[y], slackx[y]);
-//                    }
-//                }
-//            }
-//        if (y < n) break;
-//    }
-//
-//    if (y < n){
-//        maxMatch++;
-//        for (var cx = x, cy = y, ty; cx != -2; cx = prev[cx], cy = ty){
-//            ty = xy[cx];
-//            yx[cy] = cx;
-//            xy[cx] = cy;
-//        }
-//        augment();
-//    }
-//}
-//
-//function update_labels(){
-//    var x, y, delta = -1;
-//    for (y = 0; y < n; y++) {
-//        if (!T[y] && (delta == -1 || slack[y] < delta)) {
-//            delta = slack[y];
-//        }
-//    }
-//    for (x = 0; x < n; x++) {
-//        if (S[x]) lx[x] -= delta;
-//    }
-//    for (y = 0; y < n; y++) {
-//        if (T[y]) ly[y] += delta;
-//    }
-//    for (y = 0; y < n; y++) {
-//        if (!T[y])
-//            slack[y] -= delta;
-//    }
-//}
-//
-//
-//function add_to_tree(x, prevx){
-//    S[x] = true;
-//    prev[x] = prevx;
-//    for (var y = 0; y < n; y++) {
-//        if (lx[x] + ly[y] - cost[x][y] < slack[y]) {
-//            slack[y] = lx[x] + ly[y] - cost[x][y];
-//            slackx[y] = x;
-//        }
-//    }
-//}
-//
-//function hungarianMethod(){
-//    var ret = 0;
-//    initLabels();
-//    augment();
-//    for (var x = 0; x < n; x++)
-//    ret += cost[x][xy[x]];
-//    alert(ret);
-//    return ret;
-//}
-//
-//var cost = [[7,4,3],[3,1,2],[3,0,0]];
-//
-//var lx = new Array(cost.length),
-//    ly = new Array(cost.length),
-//    xy = new Array(cost.length),
-//    yx = new Array(cost.length),
-//    S = new Array(cost.length),
-//    T = new Array(cost.length),
-//    slack = new Array(cost.length),
-//    slackx = new Array(cost.length),
-//    prev = new Array(cost.length),
-//    maxMatch = 0;
-//var n = cost.length;
-//setAll(S, false);
-//setAll(T, false);
-//setAll(xy, -1);
-//setAll(yx, -1);
-//setAll(lx, 0);
-//setAll(ly, 0);
-//
-//
-//hungarianMethod();
-
 /**
- * Instanz des Hopcroft-Karp Algorithmus, erweitert die Klasse CanvasDrawer
+ * Instanz der Ungarischen Methode, erweitert die Klasse CanvasDrawer
  * @constructor
  * @augments CanvasDrawer
  * @param {Graph} p_graph Graph, auf dem der Algorithmus ausgeführt wird
@@ -182,6 +35,11 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
      * @type HungarianMethod
      */
     var algo = this;
+
+    /**
+     * Hier die Variablen vom HK-Algo
+     */
+
     /**
      * Hier die Variablen vom HK-Algo
      */
@@ -226,10 +84,10 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
      * @type Number
      * */
     var currentPath = 0;
-    /*
+    /**
      * Alle benoetigten Information zur Wiederherstellung der vorangegangenen Schritte werden hier gespeichert.
      * @type Array
-     * */
+     */
     var history = new Array();
     /**
      * Zählt die Phasen / Runden.
@@ -242,24 +100,14 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
      */
     var end = false;
     /**
-     * Wird fuer die Anzeige des Matchings am Ende des Algorithmus benoetigt.
-     * @type Boolean
-     */
-    var toggleMatchButton = true;
-    /**
      * Gibt das Statusausgabefenster an.
      */
     var statusErklaerung = "#ta_div_statusErklaerung";
-    /*
+
+    var currentDisplayStep = 0;
+    /**
      * Hier werden die Statuskonstanten definiert
-     * */
-    //const ALGOINIT = 0;
-    //const BEGIN_ITERATION = 1;
-    //const END_ITERATION = 2;
-    //const NEXT_AUGMENTING_PATH = 3;
-    //const UPDATE_MATCHING = 4;
-    //const GRAY_PATH = 5;
-    //const END_ALGORITHM = 6;
+     */
     const BEGIN = 0;
     const READY_TO_START = 1;
     const AUGMENTING_PATH_FOUND = 3;
@@ -269,11 +117,8 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
     const MATCHING_INCREASED = 7;
     const FINISHED = 8;
 
-    //var cost = [[7,4,3],[6,8,5],[9,4,4]];
     var cost = new Array();
     this.cost = cost;
-
-    var currentAugmPath = new Array();
 
     var n = Object.keys(graph.nodes).length/2;
     var lx = new Array(n),
@@ -398,10 +243,10 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
      * @method
      */
     this.fastForwardAlgorithm = function () {
-        //$("#ta_button_vorspulen").hide();
-        //$("#ta_button_stoppVorspulen").show();
-        //$("#ta_button_1Schritt").button("option", "disabled", true);
-        //$("#ta_button_Zurueck").button("option", "disabled", true);
+        $("#ta_button_vorspulen").hide();
+        $("#ta_button_stoppVorspulen").show();
+        $("#ta_button_1Schritt").button("option", "disabled", true);
+        $("#ta_button_Zurueck").button("option", "disabled", true);
         var geschwindigkeit = 200;	// Geschwindigkeit, mit der der Algorithmus ausgeführt wird in Millisekunden
 
         fastForwardIntervalID = window.setInterval(function () {
@@ -436,11 +281,18 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
      *  @method
      */
     this.nextStepChoice = function () {
-        this.addReplayStep();
+        if(currentDisplayStep < history.length){
+            currentDisplayStep++;
+            this.replayStep(currentDisplayStep);
+            this.needRedraw = true;
+            return;
+        }
+        currentDisplayStep++;
         console.log(statusID);
         $("#ta_div_statusErklaerung").text("");
         switch (statusID) {
             case BEGIN:
+                $("#ta_button_Zurueck").button("option", "disabled", false);
                 this.initLabels();
                 break;
             case READY_FOR_SEARCHING:
@@ -469,15 +321,17 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
                 break;
         }
         this.needRedraw = true;
+        this.addReplayStep();
+        console.log(history);
     };
 
     this.initLabels = function(){
-        setAll(S, false);
-        setAll(T, false);
-        setAll(xy, -1);
-        setAll(yx, -1);
-        setAll(lx, 0);
-        setAll(ly, 0);
+        this.setAll(S, false);
+        this.setAll(T, false);
+        this.setAll(xy, -1);
+        this.setAll(yx, -1);
+        this.setAll(lx, 0);
+        this.setAll(ly, 0);
         for (var x = 0; x < n; x++) {
             for (var y = 0; y < n; y++) {
                 if(this.cost[x][y] > lx[x]){
@@ -495,7 +349,6 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
     };
 
     this.augment = function() {
-        //resetEdgeLayout();
         if (maxMatch == cost.length) {
             statusID = FINISHED;
             console.log("FINISHED");
@@ -507,9 +360,9 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
         x, y, root = -1;
         q = new Array(n);
         wr = 0, rd = 0;
-        setAll(S, false);
-        setAll(T, false);
-        setAll(prev, -1);
+        this.setAll(S, false);
+        this.setAll(T, false);
+        this.setAll(prev, -1);
         for (x = 0; x < n; x++) {
             if (xy[x] == -1) {
                 q[wr++] = root = x;
@@ -528,44 +381,9 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
         console.log("READY_FOR_SEARCHING");
         $("#ta_div_statusErklaerung").text("Die Wurzel des alternierenden Baums wurde bestimmt.");
         return READY_FOR_SEARCHING;
-        //while (true) {
-        //    if(this.buildTree()){
-        //        return AUGMENTING_PATH_FOUND;
-        //    }
-        //    return AUGMENTING_PATH_NOT_FOUND;
-            //this.update_labels();
-            //this.findAugmentPathAfterLabeling();
-                        //wr = rd = 0;
-                        //for (y = 0; y < n; y++)
-                        //    if (!T[y] && slack[y] == 0) {
-                        //        if (yx[y] == -1) {
-                        //            x = slackx[y];
-                        //            break;
-                        //        } else {
-                        //            T[y] = true;
-                        //            if (!S[yx[y]]) {
-                        //                q[wr++] = yx[y];
-                        //                this.add_to_tree(yx[y], slackx[y]);
-                        //            }
-                        //        }
-                        //    }
-                        //if (y < n) break;
-        //}
-        //this.increaseMatching();
-            //if (y < n) {
-            //    maxMatch++;
-            //    for (var cx = x, cy = y, ty; cx != -2; cx = prev[cx], cy = ty) {
-            //        ty = xy[cx];
-            //        yx[cy] = cx;
-            //        xy[cx] = cy;
-            //    }
-            //    return MATCHING_INCREASED;//this.augment();
-            //}
     };
 
     this.increaseMatching = function(){
-        //if (y < n) {
-        //    resetEdgeLayout();
         resetNodeLayout();
         showEqualityGraph(lx, ly);
         maxMatch++;
@@ -580,8 +398,6 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
         console.log("MATCHING_INCREASED");
         $("#ta_div_statusErklaerung").text("Matching wurde vergrößert.");
         return MATCHING_INCREASED;//this.augment();
-        //}
-        //return -1;
     };
 
     this.tryToFindAugmentingPath = function(){
@@ -592,7 +408,6 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
             $("#ta_div_statusErklaerung").text("Augmentationsweg wurde gefunden.");
             return AUGMENTING_PATH_FOUND;
         }
-        //resetNodeLayout();
         statusID = AUGMENTING_PATH_NOT_FOUND;
         console.log("AUGMENTING_PATH_NOT_FOUND");
         $("#ta_div_statusErklaerung").text("Augmentationsweg wurde nicht gefunden.");
@@ -611,9 +426,6 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
                 }
             }
             if (y < n) {
-                //currentAugmPath[currentAugmPath.length] = x;
-                //currentAugmPath[currentAugmPath.length] = y;
-                //console.log(currentAugmPath);
                 return true;
             } //augmenting path found
         }
@@ -621,7 +433,6 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
     };
 
     this.findAugmentPathAfterLabeling = function(){
-        //resetNodeLayout();
         wr = rd = 0;
         for (y = 0; y < n; y++)
             if (!T[y] && slack[y] == 0) {
@@ -637,10 +448,7 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
                 }
             }
         if (y < n){
-            //currentAugmPath[currentAugmPath.length] = x;
-            //currentAugmPath[currentAugmPath.length] = y;
             showAugmentingPath(x, y, prev, xy, yx);
-            //console.log(currentAugmPath);
             statusID = AUGMENTING_PATH_FOUND;
             console.log("AUGMENTING_PATH_FOUND");
             $("#ta_div_statusErklaerung").text("Augmentationsweg wurde gefunden.");
@@ -665,7 +473,6 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
     };
 
     this.update_labels = function(){
-        //resetNodeLayout();
         var x, y, delta = -1;
         for (y = 0; y < n; y++) {
             if (!T[y] && (delta == -1 || slack[y] < delta)) {
@@ -688,74 +495,13 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
         $("#ta_div_statusErklaerung").text("Markierungen sowie Gleichheitsgraph wurden aktualisiert.");
         return LABELS_UPDATED;
     };
-    /*
-     * Methoden fuer die Visualisierung.
-     * */
-    var setEdgeMatched = function(edge){
-        var MATCHED_EDGE_COLOR = "DarkBlue";
-        edge.setLayout("lineColor", MATCHED_EDGE_COLOR);
-        edge.setLayout("lineWidth", global_Edgelayout.lineWidth*1.3);
-    };
-    var setEdgeNotMatched = function(edge){
-        edge.setLayout("lineColor", global_Edgelayout.lineColor);
-        edge.setLayout("lineWidth", global_Edgelayout.lineWidth);
-    };
-    var setNodeMatched = function(node){
-        var MATCHED_NODE_COLOR = const_Colors.NodeFillingHighlight;
-        node.setLayout('fillStyle',MATCHED_NODE_COLOR);
-        node.setLayout('borderColor',global_NodeLayout.borderColor);
-    };
-    var setNodeNotMatched = function(node){
-        node.restoreLayout();
-    };
-    var highlightNode = function(node){
-        node.setLayout('borderColor',"Navy");
-        //node.setLayout('borderColor',const_Colors.NodeBorderHighlight);
-        node.setLayout('borderWidth',global_NodeLayout.borderWidth*1.5);
-    };
-    var highlightEdge = function(edge){
-        edge.setLayout("lineWidth", global_Edgelayout.lineWidth*2.4);
-    };
-    var highlightFreeNode = function(node){
-        node.setLayout('borderWidth',global_NodeLayout.borderWidth * 2);
-        node.setLayout('fillStyle',"SteelBlue ");
-    };
-    var hideEdge = function(edge) {
-        edge.setLayout("lineWidth", global_Edgelayout.lineWidth * 0.3);
-    };
-    var hideNode = function(node){
-        node.setLayout('fillStyle',"DarkGray");
-        node.setLayout('borderWidth',global_NodeLayout.borderWidth);
-        node.setLayout('borderColor',"Gray");
-    };
 
-    /*
-     * Hebt den Augmentationsweg hervor.
-     * @method
-     * */
-    this.highlightPath = function(path){
-        //alle Kanten ein wenig in den Hintergrund
-        //for(var e in graph.edges) graph.edges[e].setLayout("lineWidth", global_Edgelayout.lineWidth * 0.9);
-        //hervorheben des Augmentationsweges(Knoten und Kanten)
-        for(var n = 0; n < path.length; n=n+2){
-            var node = path[n];
-            highlightNode(node);
-            if(n < path.length - 1){
-                var edge = path[n+1];
-                highlightEdge(edge);
-            }
+    this.setAll = function (arr, val) {
+        var i, n = arr.length;
+        for (i = 0; i < n; ++i) {
+            arr[i] = val;
         }
-        //die freien Knoten hervorheben(erster und letzter Knoten auf dem Weg)
-        highlightFreeNode(path[0]);
-        highlightFreeNode(path[path.length-1]);
-
-        //statuserklaerung
-        $(statusErklaerung).html('<h3>'+iteration+'. '+LNG.K('textdb_text_iteration')+'</h3>'
-        + "<h3> "+LNG.K('textdb_msg_path_highlight')+"</h3>"
-        + "<p> "+LNG.K('textdb_msg_path_highlight_1')+"<p>");
-        statusID = UPDATE_MATCHING;
     };
-
 
     /**
      * Zeigt Texte und Buttons zum Ende des Algorithmus
@@ -777,7 +523,8 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
      * @method
      */
     this.previousStepChoice = function() {
-        this.replayStep();
+        currentDisplayStep--;
+        this.replayStep(currentDisplayStep);
         this.needRedraw = true;
     };
 
@@ -785,7 +532,7 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
     this.addReplayStep = function() {
         var nodeProperties = {};
         for(var key in graph.nodes) {
-            nodeProperties[graph.nodes[key].getNodeID()] = {edge: JSON.stringify(graph.nodes[key].getLayout())};
+            nodeProperties[graph.nodes[key].getNodeID()] = {edge: JSON.stringify(graph.nodes[key].getLayout()), label: graph.nodes[key].getLabel()};
         }
         var edgeProperties = {}
         for(var key in graph.edges) {
@@ -803,14 +550,23 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
             "matched": jQuery.extend({},matched),
             "disjointPaths": jQuery.extend([],disjointPaths),
             "currentPath": currentPath,
+            //"lx":jQuery.extend({},lx),
+            //"ly":jQuery.extend({},ly),
             "htmlSidebar": $(statusErklaerung).html()
+
+
+            //"previousStatusId": statusID,
+            //"nodeProperties": nodeProperties,
+            //"edgeProperties": edgeProperties,
+            //"lx":jQuery.extend({},lx),
+            //"ly":jQuery.extend({},ly),
+            //"htmlSidebar": $(statusErklaerung).html()
         });
-        //console.log("Current History Step: ", history[history.length-1]);
     };
 
-    this.replayStep = function() {
-        if(history.length > 0){
-            var oldState = history.pop();
+    this.replayStep = function(current) {
+        if(current > 0){
+            var oldState = history[current - 1];//history.pop();
             //console.log("Replay Step", oldState);
             statusID = oldState.previousStatusId;
             matching = oldState.matching;
@@ -824,41 +580,22 @@ function HungarianMethod(p_graph,p_canvas,p_tab) {
             $("#ta_div_statusErklaerung").html(oldState.htmlSidebar);
             for(var key in oldState.nodeProperties) {
                 graph.nodes[key].setLayoutObject(JSON.parse(oldState.nodeProperties[key].edge));
-                //graph.nodes[key].setLabel(oldState.nodeProperties[key].label);
+                graph.nodes[key].setLabel(oldState.nodeProperties[key].label);
             }
             for(var key in oldState.edgeProperties) {
                 graph.edges[key].setLayoutObject(JSON.parse(oldState.edgeProperties[key].edge));
                 //graph.edges[key].setAdditionalLabel(oldState.edgeProperties[key].label);
             }
-        }
-        if(history.length == 0){
+            $("#ta_button_Zurueck").button("option", "disabled", false);
+        }else{
             $("#ta_button_Zurueck").button("option", "disabled", true);
+            //TODO add initial state setting
         }
         if(end){
             end = false;
             $("#ta_button_1Schritt").button("option", "disabled", false);
             $("#ta_button_vorspulen").button("option", "disabled", false);
         }
-    };
-
-    this.setOutputFenster = function(fenster){
-        statusErklaerung = fenster;
-    };
-    this.getShortestPathLength = function(){
-        return shortestPathLength;
-    };
-    this.isMatched = function (node){
-        return (matched[node.getNodeID()] != null);
-    };
-    this.getPartner = function (node){
-        return matched[node.getNodeID()];
-    };
-    this.getMatching = function(){return matching};
-    this.getPath = function () {return disjointPaths[currentPath];};
-    this.getGraph = function () {return graph;};
-    this.startNewIteration = function() {
-        this.endIteration();
-        this.nextStepChoice();
     };
 }
 
